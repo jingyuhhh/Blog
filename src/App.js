293c14e9d92133b3836574ff972ce84a23816105
火山火山 <2741547963@qq.com>
2023-05-08 22:50:10 +0800

    准备做一个博客hh

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Navbar">
+        <div className="logo">Volcano's Blog</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
